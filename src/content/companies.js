06f3793d62df2c1a9e293540d47537f8b26df7c4
Ranindu15 <ranindujayasooriya@gmail.com>
2020-07-10 13:57:24 +0530

    Update Company content

--- src/content/companies.js
+++ src/content/companies.js
@@ -43,43 +43,88 @@ function Companies(){
                     </div>
                 </div>
 
-                <div class="container-start" style={{padding:'30px 10px' }}>
-                <table class="table table-hover" style={{width:'60em', backgroundColor:'#ffff'}}>
-                    <thead>
-                    <tr>
-                        <th style={{width:'5em'}}> Logo</th>
-                        <th style={{width:'20em'}}>Company Name</th>
-                        <th style={{width:'20em'}}>Email</th>
-                        <th>Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>John</td>
-                        <td>Doe</td>
-                        <td>john@example.com</td>
-                        <button className="companyview-btn">View</button> <button className="companydel-btn">Delete</button>
-                    </tr>
-                    <tr>
-                        <td>Mary</td>
-                        <td>Moe</td>
-                        <td>mary@example.com</td>
-                        <button className="companyview-btn">View</button> <button className="companydel-btn">Delete</button>
-                    </tr>
-                    <tr>
-                        <td>July</td>
-                        <td>Dooley</td>
-                        <td>july@example.com</td>
-                        <button className="companyview-btn">View</button> <button className="companydel-btn">Delete</button>
-                    </tr>
-                    <tr>
-                        <td>John</td>
-                        <td>Doe</td>
-                        <td>john@example.com</td>
-                        <button className="companyview-btn">View</button> <button className="companydel-btn">Delete</button>
-                    </tr>
-                    </tbody>
-                </table>
+                <div class="d-flex align-items-start flex-row">
+                <div class="container-start" style={{padding:'30px 10px', width:'80em' }}>
+                    <table class="table table-hover" style={{width:'60em', backgroundColor:'#ffffff'}}>
+                    <caption style={{captionSide:'top', fontSize:'1.5em'}}><span class="material-icons" style={{verticalAlign:'middle'}}>launch</span>&nbsp; Recent Companies</caption>
+                        <thead>
+                        <tr>
+                            <th > # </th>
+                            <th><span class="material-icons" style={{verticalAlign:'middle'}}>business</span>&nbsp;Company Name</th>
+                            <th><span class="material-icons" style={{verticalAlign:'middle'}}>email</span> Email</th>
+                            <th><span class="material-icons" style={{verticalAlign:'middle'}}>build</span>&nbsp;Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr style={{height:'1em'}}>
+                            <td><input type="checkbox" name="name1" /></td>
+                            <td>Doe</td>
+                            <td>john@example.com</td>
+                            <td><button className="companyview-btn">View</button>&nbsp;<button className="companydel-btn">Delete</button>
+                            &nbsp;<button className="companymsg-btn">Message</button></td>
+                        </tr>
+                        <tr>
+                        <td><input type="checkbox" name="name1" /></td>
+                            <td>Moe</td>
+                            <td>mary@example.com</td>
+                            <td><button className="companyview-btn">View</button>&nbsp;<button className="companydel-btn">Delete</button>
+                            &nbsp;<button className="companymsg-btn">Message</button></td>
+                        </tr>
+                        <tr>
+                        <td><input type="checkbox" name="name1" /></td>
+                            <td>Moe</td>
+                            <td>mary@example.com</td>
+                            <td><button className="companyview-btn">View</button>&nbsp;<button className="companydel-btn">Delete</button>
+                            &nbsp;<button className="companymsg-btn">Message</button></td>
+                        </tr>
+                        <tr>
+                        <td><input type="checkbox" name="name1" /></td>
+                            <td>Moe</td>
+                            <td>mary@example.com</td>
+                            <td><button className="companyview-btn">View</button>&nbsp;<button className="companydel-btn">Delete</button>
+                            &nbsp;<button className="companymsg-btn">Message</button></td>
+                        </tr>
+                        <tr>
+                        <td><input type="checkbox" name="name1" /></td>
+                            <td>Moe</td>
+                            <td>mary@example.com</td>
+                            <td><button className="companyview-btn">View</button>&nbsp;<button className="companydel-btn">Delete</button>
+                            &nbsp;<button className="companymsg-btn">Message</button></td>
+                        </tr>
+                        <tr>
+                        <td><input type="checkbox" name="name1" /></td>
+                            <td>Moe</td>
+                            <td>mary@example.com</td>
+                            <td><button className="companyview-btn">View</button>&nbsp;<button className="companydel-btn">Delete</button>
+                            &nbsp;<button className="companymsg-btn">Message</button></td>
+                        </tr>
+                        <tr>
+                        <td><input type="checkbox" name="name1" /></td>
+                            <td>Dooley</td>
+                            <td>july@example.com</td>
+                            <td><button className="companyview-btn">View</button>&nbsp;<button className="companydel-btn">Delete</button>
+                            &nbsp;<button className="companymsg-btn">Message</button></td>
+                        </tr>
+                        <tr>
+                        <td><input type="checkbox" name="name1" /></td>
+                            <td>Doe</td>
+                            <td>john@example.com</td>
+                            <td><button className="companyview-btn">View</button>&nbsp;<button className="companydel-btn">Delete</button>
+                            &nbsp;<button className="companymsg-btn">Message</button></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                        <div class="card " style={{ height:'20rem',width:'24em', margin:'5.5em 1.5em'}}>
+                            <div class="card-body">
+                                <h3 class="card-title" style={{fontSize:'50px'}}>
+                                <span class="material-icons">corporate_fare</span>Total Companies</h3>
+                                <p class="card-text" style={{fontFamily:'sans-serif', fontSize:'23px'}}>25</p><hr/>
+                                    <button type="button" class="btn btn-secondary">Send Message</button>
+                            </div>
+                        </div>
+
                 </div>
 
             </div>
